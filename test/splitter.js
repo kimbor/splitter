5b662543354ea5cb2284aebe81ed200ba36f0fc3
Kim Rader <kimrader@gmail.com>
2018-02-18 20:38:36 -0800

    "should send coin correctly" works for happy path

--- test/splitter.js
+++ test/splitter.js
@@ -3,14 +3,16 @@ Promise.promisifyAll(web3.eth, { suffix: "Promise" });
 var Splitter = artifacts.require("./Splitter.sol");
 
 contract('Splitter', function(accounts) {
-  console.log(accounts);
-  var splitter;
+  console.log("alice: " + accounts[0]);
+  console.log("bob: " + accounts[1]);
+  console.log("carol: " + accounts[2]);
+  var splitterContractInstance;
 
-  beforeEach("deploy and prepare", function() {
-    console.log("running beforeEach");
+  before("deploy and prepare", function() {
+    console.log("running before");
     return Splitter.new(web3.eth.accounts[1], web3.eth.accounts[2], {from:web3.eth.accounts[0]})
       .then(instance => {
-        splitter = instance;
+        splitterContractInstance = instance;
         console.log("splitter initialized");
       });
   });
@@ -19,48 +21,49 @@ contract('Splitter', function(accounts) {
 
   it("should send coin correctly", function() {
     console.log("running 'should send coin correctly' test");
-    console.log("contract address: " + splitter.address);
+    console.log("contract address: " + splitterContractInstance.address);
 
     var alice_starting_balance = web3.eth.getBalance(web3.eth.coinbase);
     console.log("Alice starting balance: " + alice_starting_balance.toString(10));
 
-    var amount = 10;
+//    var amount = 10;
+    var amount = 100000000000000;
     var splitAmount = amount / 2;
+    console.log("amount to split: " + amount);
+    console.log("split amount: " + splitAmount);
 
     var contract_starting_balance;
     var bob_starting_balance;
     var carol_starting_balance;
 
-    // return Splitter.deployed().then(function(instance) {
-    //   splitter = instance;
-    return web3.eth.getBalancePromise(splitter.address)
+    return web3.eth.getBalancePromise(splitterContractInstance.address)
     .then(balance => {
       contract_starting_balance = balance.toNumber();
       console.log("Contract starting balance: " + contract_starting_balance.toString(10));
-      return splitter.getBalanceBob.call();
+      return splitterContractInstance.getBalanceBob.call();
     }).then(balance => {
       bob_starting_balance = balance.toNumber();
       console.log("Bob starting balance: " + bob_starting_balance.toString(10));
-      return splitter.getBalanceCarol.call();
+      return splitterContractInstance.getBalanceCarol.call();
     }).then(balance => {
       carol_starting_balance = balance.toNumber();
       console.log("Carol starting balance: " + carol_starting_balance.toString(10));
       console.log("sending first split");
-      return splitter.split({value:amount, gas:3000000});
+      return splitterContractInstance.split({value:amount, gas:3000000});
     }).then(success => {
-      console.log("success: " + success.toString());
+      console.log("success: " + (success ? "true" : "false"));
       assert(success, "split was not successful");
-      return splitter.getBalanceBob.call();
+      return splitterContractInstance.getBalanceBob.call();
     }).then(function(balance) {
       var bob_ending_balance = balance.toNumber();
       console.log("Bob ending balance: " + bob_ending_balance);
       assert.equal(bob_ending_balance, bob_starting_balance + splitAmount, "Amount wasn't correctly sent to bob");
-      return splitter.getBalanceCarol.call();
+      return splitterContractInstance.getBalanceCarol.call();
     }).then(function(balance) {
       var carol_ending_balance = balance.toNumber();
       assert.equal(carol_ending_balance, carol_starting_balance + splitAmount, "Amount wasn't correctly sent to carol");
       console.log("Carol ending balance: " + carol_ending_balance);
-      return web3.eth.getBalancePromise(splitter.address);
+      return web3.eth.getBalancePromise(splitterContractInstance.address);
     }).then(function(balance) {
       var contract_ending_balance = balance.toNumber();
       console.log("Contract ending balance: " + contract_ending_balance.toString(10));
@@ -68,28 +71,32 @@ contract('Splitter', function(accounts) {
     });
   });
 
-/*
   it("should withdraw coin correctly", function() {
     console.log("running 'should withdraw coin correctly' test");
 
-    var bob_starting_balance = 0;
-    var bob_ending_balance;
-    var amount_to_withdraw = 2;
+    var bob_starting_balance;
+    var contract_starting_balance;
+    var amount_to_withdraw = 1;
 
     console.log("withdraw initialization");
-    return Splitter.deployed().then(function(instance) {
-      splitter = instance;
-      return web3.eth.getBalancePromise(splitter.address);
-    }).then(contract_starting_balance => {
+
+    return web3.eth.getBalancePromise(splitterContractInstance.address)
+    .then(balance => {
+      contract_starting_balance = balance.toNumber();
       console.log("Contract starting balance: " + contract_starting_balance.toString(10));
-      return splitter.getBalanceBob().call();
-    }).then(bob_starting_balance => {
-      console.log("Bob starting balance: " + bob_starting_balance.toNumber().toString(10));
+      return splitterContractInstance.getBalanceBob.call();
+    }).then(balance => {
+      bob_starting_balance = balance.toNumber();
+      console.log("Bob starting balance: " + bob_starting_balance.toString(10));
       console.log("sending bob withdrawal");
-      return splitter.withdraw(5, {from: web3.eth.accounts[1], gas:1000000});
+      return splitterContractInstance.withdraw(amount_to_withdraw, {from: web3.eth.accounts[1], gas:100000 });
     }).then(txObject => {
-      assert.equal(bob_ending_balance, bob_starting_balance.minus(amount_to_withdraw), "Amount wasn't correctly sent to bob");
+      console.log("withdraw successful");
+      return splitterContractInstance.getBalanceBob.call();
+    }).then(balance => {
+      var bob_ending_balance = balance.toNumber();
+      console.log("Bob ending balance: " + bob_ending_balance);
+      assert.equal(bob_ending_balance, bob_starting_balance - amount_to_withdraw, "Amount wasn't correctly sent to bob");
     });
   });
-  */
  });
